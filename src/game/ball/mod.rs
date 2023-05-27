8f664642f9e6b7a21c56a7b6c7c26dcd799233a3
ontingleung <ontingleung@tutamail.com>
2023-05-27 13:59:52 +0100

    confine ball

--- src/game/ball/mod.rs
+++ src/game/ball/mod.rs
@@ -9,6 +9,7 @@ impl Plugin for BallPlugin {
     fn build(&self, app: &mut App) {
         app
             .add_startup_system(spawn_ball)
-            .add_system(ball_movement);
+            .add_system(ball_movement)
+            .add_system(confine_ball_movement);
     }
 }
--- src/game/ball/systems.rs
+++ src/game/ball/systems.rs
@@ -3,7 +3,7 @@ use bevy::prelude::*;
 use bevy::window::PrimaryWindow;
 use rand::prelude::*;
 
-// pub const BALL_SIZE: f32 = 64.0;
+pub const BALL_SIZE: f32 = 64.0;
 pub const BALL_SPEED: f32 = 250.0;
 
 pub fn spawn_ball(
@@ -32,5 +32,38 @@ pub fn ball_movement(
     for (mut transform, ball) in ball_query.iter_mut() {
         let direction = Vec3::new(ball.direction.x, ball.direction.y, 0.0);
         transform.translation += direction * BALL_SPEED * time.delta_seconds();
+    }
+}
+
+pub fn confine_ball_movement(
+    mut ball_query: Query<&mut Transform, With<Ball>>,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+) {
+    let window = window_query.get_single().unwrap();
+
+    let half_ball_size = BALL_SIZE / 2.0;
+    let x_min = 0.0 + half_ball_size;
+    let x_max = window.width() - half_ball_size;
+    let y_min = 0.0 + half_ball_size;
+    let y_max = window.height() - half_ball_size;
+
+    for mut transform in ball_query.iter_mut() {
+        let mut translation = transform.translation;
+
+        // Bound the enemy x position
+        if translation.x < x_min {
+            translation.x = x_min;
+        } else if translation.x > x_max {
+            translation.x = x_max;
+        }
+
+        // Bound the ball y position
+        if translation.y < y_min {
+            translation.y = y_min;
+        } else if translation.y > y_max {
+            translation.y = y_max;
+        }
+
+        transform.translation = translation;
     }
 }
